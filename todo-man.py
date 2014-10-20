f56deef17c52fc59f6760beaa5cec9a3fe7b2be2
Rylan <rylans@gmail.com>
2014-10-20 15:35:31 -0700

    Make Todo class

--- todo-man.py
+++ todo-man.py
@@ -1,5 +1,15 @@
 import argparse
 import os
+#TODO: put in docstring
+
+class Todo:
+  def __init__(self, filepath, line_number, text):
+    self.filepath = filepath
+    self.line_number = line_number
+    self.text = text
+
+  def __repr__(self):
+    return '[' + ','.join([self.filepath, str(self.line_number), self.text]) + ']'
 
 def get_files(suffix):
   file_list = []
@@ -9,10 +19,24 @@ def get_files(suffix):
 	file_list.append(os.path.join(root, f))
   return file_list
 
+def get_todos(file_list):
+  todo_list = []
+  for file_ in file_list:
+    with open(file_, 'r') as f:
+      i = 0
+      for line in f.readlines():
+	i += 1
+	if 'TODO:' in line:
+	  if len(line.strip().split('TODO')[0]) <= 2:
+	    todo = line.split('TODO:')[1].strip()
+	    todo_list.append(Todo(file_, i, todo))
+  return todo_list
+
 def main():
   parser = argparse.ArgumentParser(description='Process.')
   parser.add_argument('out', metavar='O', nargs='?', default='TODO.md', help='The markdown file to write TODOs to')
   parser.add_argument('file_type', metavar='T', nargs=1, help='The file suffix of source code files (ex: py, js, java)')
+  #TODO: Make an argument '-f' to force the output file to get overwritten
   args = parser.parse_args()
   print args.out
   print args.file_type[0]
@@ -20,7 +44,9 @@ def main():
   output_md = args.out
   file_suffix = args.file_type[0]
 
-  print get_files(file_suffix)
+  files = get_files(file_suffix)
+  print get_todos(files)
 
 if __name__ == '__main__':
+  #TODO: Import logging and use it
   main()
