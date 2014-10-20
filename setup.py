3f869e81a8c5e8f62146bc09b0c316fbc1327799
Rylan <rylans@gmail.com>
2014-10-20 16:23:15 -0700

    Fix typo in import statement

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-form setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(name='todo-man',
       version='0.0.1',
       author='Rylan Santinon',
+      description='Grab TODOs in your source code them and put them into a markdown document',
       license='Apache',
       packages=['todo-man'])
