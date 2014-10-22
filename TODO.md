9b231231f30f2b8bd5ef36ffc70945d8cd664159
Rylan <rylans@gmail.com>
2014-10-22 14:47:43 -0700

    Make SourceCodeLine class Add test for repr Add tests for has_todo function Include Docstring comment

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 #TODO List
 - [ ] Level 0 todo (./todo-man/todo-man.py:11)
-- [ ] Sort todo_list by file and line number (./todo-man/todo-man.py:97)
-- [ ] USED FOR TESTING (./todo-man/todo-man.py:108)
-- [ ] Make an argument '-f' to force the output file to get overwritten (./todo-man/todo-man.py:148)
-- [ ] Import logging and use it (./todo-man/todo-man.py:155)
+- [ ] Sort todo_list by file and line number (./todo-man/todo-man.py:149)
+- [ ] USED FOR TESTING (./todo-man/todo-man.py:160)
+- [ ] Make an argument '-f' to force the output file to get overwritten (./todo-man/todo-man.py:200)
+- [ ] Import logging and use it (./todo-man/todo-man.py:207)
--- todo-man/todo-man.py
+++ todo-man/todo-man.py
@@ -19,6 +19,61 @@ class Todo: #TODO: subclass object explicitly?
   def __repr__(self):
     return '[' + ','.join([self.filepath, self.line_number, self.text]) + ']'
 
+class SourceCodeLine:
+  '''A line of code in a file'''
+  todo_string = 'TODO:'
+
+  def __init__(self, line, line_number, filename):
+    self.line = line
+    self.line_number = line_number
+    self.filename = filename
+
+  def len_before(self, span):
+    before = self.line[:span[0]].strip()
+    if len(before) <= 2:
+      return True
+    return False
+
+  def has_todo(self):
+    """Returns true if this contains a valid TODO comment
+
+    >>> SourceCodeLine('#todo: rethink this', 39, 'abc.py').has_todo()
+    True
+
+    >>> SourceCodeLine('//ToDo: rewrite this', 43, 'abc.py').has_todo()
+    True
+
+    >>> SourceCodeLine('public class todo(){', 899, 'abc.java').has_todo()
+    False
+
+    >>> SourceCodeLine('while(x>15) {', 26, 'abc.java').has_todo()
+    False
+    """
+    re_search = re.search(SourceCodeLine.todo_string, self.line, re.IGNORECASE)
+    if re_search and self.len_before(re_search.span()):
+      return True
+    return False
+
+  def get_todo(self):
+    if not self.has_todo():
+      raise Exception("This line has no TODO")
+    re_search = re.search(SourceCodeLine.todo_string, self.line, re.IGNORECASE)
+    span = re_search.span()
+    after = self.line[span[1]:].strip()
+    return Todo(self.filename, self.line_number, after)
+
+  def __repr__(self):
+    """Object representation of SourceCodeLine
+
+    >>> print SourceCodeLine('public static void', 14, 'mydir/class.java')
+    SourceCodeLine(line='public static void', line_number=14, filename='mydir/class.java')
+
+    >>> sc = SourceCodeLine('for i in xrange(20)', 243, './dir1/dir2/x.py')
+    >>> repr(sc) == repr(eval(repr(sc)))
+    True
+    """
+    return "SourceCodeLine(line=%r, line_number=%r, filename=%r)" % (self.line, self.line_number, self.filename)
+
 def in_insensitive(item, item_list):
   """Returns true if item is in item_list by comparing without case
 
@@ -69,19 +124,17 @@ def get_files(suffix):
   return [file for file in file_list if file not in ignored_files]
 
 def get_todos(file_list):
+  """Get TODOs from a list of input files"""
   todo_list = []
   for file_ in file_list:
     with open(file_, 'r') as f:
       i = 0
       for line in f.readlines():
 	i += 1
-	regexp_search = re.search('TODO:', line, re.IGNORECASE)
-	if regexp_search:
-	  span = regexp_search.span()
-	  before = line[:span[0]]
-	  after = line[span[1]:].strip()
-	  if len(before.strip()) <= 2:
-	    todo_list.append(Todo(file_, i, after))
+
+	sc_line = SourceCodeLine(line, i, file_)
+	if sc_line.has_todo():
+	  todo_list.append(sc_line.get_todo())
   return todo_list
 
 def write_todos(todo_list, output_file, is_md):
